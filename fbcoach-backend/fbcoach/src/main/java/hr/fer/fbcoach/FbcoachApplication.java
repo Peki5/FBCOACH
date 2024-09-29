865c171d9f906a8dd7e2c6eb386f7f85d2b31cf7
Domagoj Pernar <do.pernar@gmail.com>
2024-09-29 16:11:19 +0200

    ready for deploy fix5

--- fbcoach-backend/fbcoach/src/main/java/hr/fer/fbcoach/FbcoachApplication.java
+++ fbcoach-backend/fbcoach/src/main/java/hr/fer/fbcoach/FbcoachApplication.java
@@ -2,8 +2,12 @@ package hr.fer.fbcoach;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "hr.fer.fbcoach.repository")
+@EntityScan(basePackages = "hr.fer.fbcoach.model")
 public class FbcoachApplication {
 
 	public static void main(String[] args) {
